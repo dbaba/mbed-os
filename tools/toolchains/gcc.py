0c1b681b84d08769639f58558ff5dd7bae1b5140
Daisuke Baba <baba.daisuke@gmail.com>
2016-10-03 11:51:25 +0900

    Apply mbed_lib_rev127

--- tools/toolchains/gcc.py
+++ tools/toolchains/gcc.py
@@ -1,13 +1,10 @@
 """
 mbed SDK
 Copyright (c) 2011-2013 ARM Limited
-
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
-
     http://www.apache.org/licenses/LICENSE-2.0
-
 Unless required by applicable law or agreed to in writing, software
 distributed under the License is distributed on an "AS IS" BASIS,
 WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -283,13 +280,13 @@ class GCC_ARM(GCC):
         GCC.__init__(self, target, options, notify, macros, silent, TOOLCHAIN_PATHS['GCC_ARM'], extra_verbose=extra_verbose)
 
         # Use latest gcc nanolib
-        if "std-lib" in self.options:
+        if "big-build" in self.options:
             use_nano = False
-        elif "small-lib" in self.options:
+        elif "small-build" in self.options:
             use_nano = True
-        elif target.default_lib == "std":
+        elif target.default_build == "standard":
             use_nano = False
-        elif target.default_lib == "small":
+        elif target.default_build == "small":
             use_nano = True
         else:
             use_nano = False
